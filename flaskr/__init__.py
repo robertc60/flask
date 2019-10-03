fbec5361ac75664862302500be49bce701e735c3
Robert Clarkson <robertc60@live.com>
2019-10-03 17:14:27 +0100

    eod 3/10/19

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,9 +2,10 @@ import os
 
 from flask import Flask
 
+
 def create_app(test_config=None):
-    #create and config the app
-    app =  Flask(__name__, instance_relative_config=True)
+#create and config the app
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -28,4 +29,18 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
+
+
+
+
